f987ffb67b24d0ea099dd3139ee3b9f91013c0dc
jvazqu61 <47464954+jvazqu61@users.noreply.github.com>
2021-12-21 10:27:11 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 /* Author: Juan Vazquez
 * Last modified: 4-26-21
-* CS 474: Project 4
 * SPL interpreter in C++ -> Simple Programming Language Interpreter that has a set of instructions.
 *                    SPL has just two data structures, integer numbers and linked lists. Linked
 *                    list elements can be integers or linked lists themselves. Identifiers must be
